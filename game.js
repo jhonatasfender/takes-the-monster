945e834a2e95b3357314518b893a7ddf06bd322d
root <root@debian.debian>
2016-06-02 02:49:14 -1100

    brick

--- game.js
+++ game.js
@@ -1,3 +1,8 @@
+/*
+ * 
+ * http://www.mathpuzzle.com/Solution.htm
+ * 
+ */
 // Create the canvas
 var w = window, d = document, e = d.documentElement, g = d.getElementsByTagName('body')[0],
         canvas = d.createElement("canvas"), ctx = canvas.getContext("2d");
@@ -6,6 +11,26 @@ var w = window, d = document, e = d.documentElement, g = d.getElementsByTagName(
 canvas.width = 512;
 canvas.height = 480;
 d.body.appendChild(canvas);
+// brick
+var bkReady = false, bkImage = new Image(), mapWall, sizeWall = {'width': 32, 'height': 32}, mapMonster;
+wall = function () {
+    mapWall = new Array(), mapMonster = new Array();
+    for (var i = 0; i < canvas.width; i = i + sizeWall.width) {
+        mapWall[i] = new Array();
+        for (var j = 0; j < canvas.height; j = j + sizeWall.height) {
+            if (i <= Math.random() * canvas.width && i >= Math.random() * canvas.height) {
+                mapWall[mapWall.length] = {'width': i, 'height': j};
+            } else {
+                mapMonster[mapMonster.length] = {w: i, h: j};
+            }
+        }
+    }
+};
+bkImage.onload = function () {
+    bkReady = true;
+};
+bkImage.src = "images/brick.png";
+wall();
 // Background image
 var bgReady = false, bgImage = new Image();
 bgImage.onload = function () {
@@ -34,21 +59,35 @@ addEventListener("keyup", function (e) {
 }, false);
 // Reset the game when the player catches a monster
 var reset = function () {
-    hero.x = monster.x == undefined ? canvas.width / 2 : monster.x;
-    hero.y = monster.y == undefined ? canvas.height / 2 : monster.y;
+    var a = mapMonster[Math.floor(Math.random() * mapMonster.length)], b = mapMonster[parseInt(mapMonster.length / 2) - 1];
+    hero.x = monster.x == undefined ? b.w : monster.x;
+    hero.y = monster.y == undefined ? b.h : monster.y;
     // Throw the monster somewhere on the screen randomly
-    monster.x = 32 + (Math.random() * (canvas.width - 64));
-    monster.y = 32 + (Math.random() * (canvas.height - 64));
-},
+    monster.x = a.w;
+    monster.y = a.h;
+}
+,
+        block = function (x, y) {
+            var retorno = true;
+            for (var i = 0; i < mapWall.length; i++) {
+                if (mapWall[i] != undefined && mapWall[i] != "") {
+                    if (x == mapWall[i].width && y == mapWall[i].height) {
+                        retorno = false;
+                    }
+                    console.log(x + " == " + mapWall[i].width + " && " + y + " == " + mapWall[i].height);
+                }
+            }
+            return retorno;
+        },
         // Update game ogjects
         update = function (modifier) {
-            if (38 in keysDown && hero.y > 5) // Player holding up
+            if (38 in keysDown && hero.y > 5 && block(hero.x, hero.y)) // Player holding up
                 hero.y -= hero.speed * modifier;
-            if (40 in keysDown && hero.y < canvas.height - 32)  // Player holding down
+            if (40 in keysDown && hero.y < canvas.height - 32 && block(hero.x, hero.y))  // Player holding down
                 hero.y += hero.speed * modifier;
-            if (37 in keysDown && hero.x > 5) // Player holding left
+            if (37 in keysDown && hero.x > 5 && block(hero.x, hero.y)) // Player holding left
                 hero.x -= hero.speed * modifier;
-            if (39 in keysDown && hero.x < canvas.width - 32) // Player holding right
+            if (39 in keysDown && hero.x < canvas.width - 32 && block(hero.x, hero.y)) // Player holding right
                 hero.x += hero.speed * modifier;
             if (hero.x <= (monster.x + 32) && monster.x <= (hero.x + 32) && hero.y <= (monster.y + 32) && monster.y <= (hero.y + 32)) {
                 ++monstersCaught;
@@ -59,6 +98,10 @@ var reset = function () {
         render = function () {
             if (bgReady)
                 ctx.drawImage(bgImage, 0, 0);
+            for (var i = 0; i < mapWall.length; i++) {
+                if (mapWall[i] != undefined)
+                    ctx.drawImage(bkImage, mapWall[i].width, mapWall[i].height, sizeWall.width, sizeWall.height);
+            }
             if (heroReady)
                 ctx.drawImage(heroImage, hero.x, hero.y);
             if (monsterReady)
